4d0ff4f45016f18e64ba036292dbe83e72404773
Fabio Borella <4450251+pencildrummer@users.noreply.github.com>
2022-02-04 10:59:23 +0100

    Updated listing plugins in settings page - Added initial support to script to create plugin for FUSE

---
+++ bin/createPlugin.js
@@ -0,0 +1,149 @@
+#! /usr/bin/env node
+const { Signale } = require('signale');
+const chalk = require('chalk');
+const { program } = require('commander');
+const prompts = require('prompts');
+const path = require('path');
+const fs = require('fs-extra');
+const os = require('os');
+const validatePackage = require('validate-npm-package-name');
+
+// Mirro console to signale
+const console = new Signale({
+  types: {
+    success: {
+      badge:  '🚀',
+      label: false,
+      color: 'green',
+      logLevel: 'info'
+    },
+    error: {
+      badge: '❌',
+      label: false,
+      color: 'red',
+      logLevel: 'error'
+    },
+    pending: { 
+      badge: '🛠',
+      label: false,
+      color: 'magenta',
+      logLevel: 'info'
+    }
+  }
+});
+
+const questions = [
+  {
+    type: 'text',
+    name: 'pluginName',
+    message: 'What\'s your plugin name?'
+  },
+  {
+    type: 'confirm',
+    name: 'isScoped',
+    message: 'Make it scoped?',
+    initial: false,
+  },
+  {
+    type: prev => prev ? 'text' : null,
+    name: 'scopeName',
+    message: 'Scope name?',
+  },
+
+  {
+    type: 'text',
+    name: 'title',
+    message: 'Human readable title'
+  },
+  {
+    type: 'text',
+    name: 'description',
+    message: 'Describe your plugin:'
+  },
+  {
+    type: 'confirm',
+    name: 'hasSettings',
+    message: 'Do you want a settings page?',
+    initial: true
+  }
+];
+
+program.name('fuse-create-plugin')
+  .description('CLI to generate starting template for FUSE plugin')
+  .version('0.0.1');
+
+function validatePluginName(name) {
+  const validationResult = validatePackage(name);
+  if (!validationResult.validForNewPackages) {
+    console.log();
+    console.error(`Unable to create plugin with name ${chalk.cyan(name)} due to naming convention restrictions:`);
+    [
+      ...(validationResult.warnings || []),
+      ...(validationResult.errors || [])
+    ].forEach(message => {
+      console.log(chalk.red('→') + ' ' + message);
+    });
+    console.log();
+
+    process.exit(1);
+  }
+}
+
+function createPlugin(name, version, opts) {
+
+  const root = path.resolve(path.join('plugins', name));
+
+  // Check plugin name if can be named that way
+  validatePluginName(name);
+
+  // Check if dir already exists
+  if (fs.existsSync(root)) {
+    console.error(`Directory for plugin named ${chalk.bold.redBright(name)} already exists.`);
+    console.log();
+    process.exit(1);
+  } else {
+    // Check dir exists or otherwise create it
+    fs.ensureDirSync(root);
+  }
+
+  console.pending(`Creating a new ${chalk.bold('FUSE')} plugin in ${chalk.magenta(root)}.`);
+  console.log();
+
+  // Prepare package content
+  let packageContent = {
+    name: name,
+    version: version,
+    license: 'MIT',
+    fuse: {}
+  };
+  if (opts.description.length) {
+    packageContent.description = opts.description;
+  }
+  if (opts.title) {
+    packageContent.fuse.title = opts.title;
+  }
+
+  fs.writeFileSync(
+    path.join(root, 'package.json'),
+    JSON.stringify(packageContent, null, 2) + os.EOL
+  )
+}
+
+(async () => {
+  const res = await prompts(questions);
+  console.log();
+
+  let pluginName = res.scopeName
+    ? path.join('@'+res.scopeName, res.pluginName) 
+    : res.pluginName
+
+  createPlugin(pluginName, '0.1', {
+    description: res.description,
+    title: res.title
+  })
+
+  console.success(`Plugin ${chalk.bold.greenBright(pluginName)} successfully created!`)
+  console.log();
+
+  process.exit(0);
+})();
--- pages/settings/plugins.js
+++ pages/settings/plugins.js
@@ -16,82 +16,6 @@ export default function PluginsSettingsPage() {
     console.log('DATA', plugins)
   }, [plugins])
 
-  // const plugins = [
-  //   {
-  //     title: 'Marlin Core',
-  //     name: 'marlin-core',
-  //     system: true,
-  //     version: '0.1',
-  //     author: '@fuse-labs',
-  //     active: true
-  //   },
-  //   {
-  //     title: 'Marlin CoreUI',
-  //     name: 'marlin-coreui',
-  //     system: true,
-  //     version: '0.1',
-  //     author: '@fuse-labs',
-  //     active: true
-  //   },
-  //   {
-  //     title: 'Camera',
-  //     name: 'camera',
-  //     system: false,
-  //     version: '0.1',
-  //     author: '@fuse-labs',
-  //     active: true,
-  //     config: true,
-  //   },
-  //   {
-  //     title: 'Marlin Extruder',
-  //     name: 'marlin-extruder',
-  //     system: true,
-  //     version: '0.1',
-  //     author: '@fuse-labs',
-  //     active: true
-  //   },
-  //   {
-  //     title: 'Marlin GCode Viewer',
-  //     name: 'marlin-gcode-viewer',
-  //     system: true,
-  //     version: '0.1',
-  //     author: '@fuse-labs',
-  //     active: true
-  //   },
-  //   {
-  //     title: 'Marlin Move',
-  //     name: 'marlin-move',
-  //     system: true,
-  //     version: '0.1',
-  //     author: '@fuse-labs',
-  //     active: true
-  //   },
-  //   {
-  //     title: 'Marlin Extra',
-  //     name: 'marlin-misc',
-  //     system: true,
-  //     version: '0.1',
-  //     author: '@fuse-labs',
-  //     active: true
-  //   },
-  //   {
-  //     title: 'Marlin Temperature',
-  //     name: 'marlin-temp',
-  //     system: true,
-  //     version: '0.1',
-  //     author: '@fuse-labs',
-  //     active: true
-  //   },
-  //   {
-  //     title: 'Marlin Terminal',
-  //     name: 'marlin-terminal',
-  //     system: true,
-  //     version: '0.1',
-  //     author: '@fuse-labs',
-  //     active: true
-  //   }
-  // ]
-
   return (
     <SettingPage>
       <DevicePluginsSettingsWidget />
@@ -106,21 +30,25 @@ export default function PluginsSettingsPage() {
               <div className="flex flex-col flex-1">
                 <div className="flex flex-row space-x-2 items-center">
                   <div className="text-gray-50">
-                    <span className="font-bold">{plugin.title}</span>
-                    <span className="text-xs text-gray-600"> by </span>
-                    <span className="text-xs text-gray-500 font-medium">{plugin.author}</span>
+                    <span className="font-bold">{plugin.fuse?.title || plugin.name}</span>
+                    {plugin.author && (
+                      <span className="text-xs text-gray-600 ml-1">
+                        <span>by </span>
+                        <span className="text-xs text-gray-500 font-medium">{plugin.author}</span>
+                      </span>
+                    )}
                   </div>
 
-                  {plugin.system && <div><span className="text-xxs font-semibold bg-blue-600 rounded-full px-2 py-0.5">CORE</span></div>}
+                  {plugin.fuse?.system && <div><span className="text-xxs font-semibold bg-blue-600 rounded-full px-2 py-0.5">CORE</span></div>}
                 </div>
                 <div className="flex flex-row items-center space-x-3">
                   <div className="text-xxs font-mono">
-                    <span className="rounded-[3px] bg-gray-800 px-1 py-0.5">{plugin.author}/{plugin.name}@{plugin.version}</span>
+                    <span className="rounded-[3px] bg-gray-800 px-1 py-0.5">{plugin.name}@{plugin.version}</span>
                   </div>
                 </div>
               </div>
 
-              {plugin.config && <div className="w-20 flex items-center justify-center">
+              {plugin.fuse?.settings && <div className="w-20 flex items-center justify-center">
                 <Link href={`/plugins/${plugin.name}/settings`}>
                   <Button rounded squared size="sm">
                     <GearIcon />
