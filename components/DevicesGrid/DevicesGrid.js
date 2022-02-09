da85af61b900bff9e99a700cc0afc2f3a71ff4fa
Fabio Borella <4450251+pencildrummer@users.noreply.github.com>
2022-02-09 17:54:47 +0100

    New devices lib - Refactor

--- components/DevicesGrid/DevicesGrid.js
+++ components/DevicesGrid/DevicesGrid.js
@@ -1,7 +1,7 @@
 import DeviceCard from "../DeviceCard/DeviceCard";
 
 export default function DevicesGrid({
-	printers: devices,
+	devices,
 }) {
 	return (
 		<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 p-3">
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,23 @@
 import DevicesGrid from "../components/DevicesGrid/DevicesGrid";
 import MainLayout from "../components/layouts/MainLayout";
+import { getDevices } from "../lib/core/devices";
 
-export default function Home() {
+export default function Home({
+  devices
+}) {
   return (
     <MainLayout>
 
-      <DevicesGrid printers={[
-        {
-          id: 1,
-          name: 'Longer LK4 PRO',
-          brand: 'LONGER',
-          model: 'LK4 Pro',
-          type: 'fdm',
-          connected: true,
-          camera: {
-            live: true
-          }
-        },
-        {
-          id: 2,
-          name: 'Elegoo MARS PRO',
-          brand: 'Elegoo',
-          model: 'MARS PRO',
-          type: 'sla',
-        },
-        {
-          id: 3,
-          name: 'CNC 3018',
-          brand: 'VEVOR',
-          model: '3018 Pro',
-          type: 'cnc',
-        }
-      ]}/>
+      <DevicesGrid devices={devices}/>
     
     </MainLayout>
   )
+}
+
+export async function getServerSideProps(ctx) {
+  return {
+    props: {
+      devices: getDevices()
+    }
+  }
 }
