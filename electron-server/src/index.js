97a5fc8a3b6f128178f9641e771ccd923bb0213b
Fabio Borella <4450251+pencildrummer@users.noreply.github.com>
2022-03-13 16:55:53 +0100

    Added initial structure for new electron-server app to be used to manager server

---
+++ electron-server/src/index.js
@@ -0,0 +1,64 @@
+const { app, BrowserWindow, Tray, Menu, nativeImage } = require('electron');
+const path = require('path');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  // eslint-disable-line global-require
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+let tray
+app.whenReady().then(_ => {
+  let imagePath = path.resolve(__dirname, 'assets', 'tray.png')
+  const icon = nativeImage.createFromPath(imagePath)
+  tray = new Tray(icon)
+
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Fuse server', type: 'normal' },
+    { type: 'separator' },
+    { label: 'Settings...', type: 'normal', click: createWindow },
+    { type: 'separator' },
+    { label: 'Quit', type: 'normal', role: 'quit' }
+  ])
+
+  tray.setToolTip('Fuse Server')
+  tray.setContextMenu(contextMenu)
+
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
