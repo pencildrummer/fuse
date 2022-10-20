0d0b7e719bc401baef33ed7edf0f7931a3b2a91d
Fabio Borella <4450251+pencildrummer@users.noreply.github.com>
2022-10-20 15:02:13 +0200

    Fixed support to load env var in client - Added favicons

--- apps/client/src/server.js
+++ apps/client/src/server.js
@@ -1,25 +1,28 @@
-const express = require('express')
-const next = require('next')
-const path = require('path')
+const express = require("express");
+const next = require("next");
+const path = require("path");
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-const app = next({ dev })
-const handle = app.getRequestHandler()
+app.prepare().then((_) => {
+  // Load port after prepare() because env vars loaded from .env files are not ready before
+  const port = parseInt(process.env.PORT, 10) || 3000;
 
-app.prepare().then(_ => {
-  const server = express()
+  const server = express();
 
   // Add custom handler to allow access to static installed file such as plugins
-  console.log('Serving /system from path ', path.join(__dirname, '/../../../..'))
-  server.use('/system', express.static(path.join(__dirname, '/../../../..')))
+  console.log(
+    "Serving /system from path ",
+    path.join(__dirname, "/../../../..")
+  );
+  server.use("/system", express.static(path.join(__dirname, "/../../../..")));
 
+  server.all("*", (req, res) => handle(req, res));
 
-  server.all('*', (req, res) => handle(req, res))
-
-  server.listen(port, err => {
-    if (err) throw err
-    console.log(`Client started on http://localhost:${port}`)
-  })
-})
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`Client started on http://localhost:${port}`);
+  });
+});
--- packages/core/server/socket-server.js
+++ packages/core/server/socket-server.js
@@ -1,43 +1,47 @@
-import express from 'express'
-import { createServer } from 'http'
-import { Server as SocketServer } from 'socket.io'
-import { DeviceManager } from './managers/index.js'
-import { getDeviceIdFromSocket } from './utils/index.js'
+import express from "express";
+import { createServer } from "http";
+import { Server as SocketServer } from "socket.io";
+import { DeviceManager } from "./managers/index.js";
+import { getDeviceIdFromSocket } from "./utils/index.js";
 
 // Socket.io server
-const expressApp = express()
-const socketServer = createServer(expressApp)
+const expressApp = express();
+const socketServer = createServer(expressApp);
 
 // Main socket server (/ namespace)
 const io = new SocketServer(socketServer, {
   cors: {
     origin: [
-      'http://localhost:3000',
-      'https://admin.socket.io',
-      'http://localhost:5000',
-      'null'
+      // TODO: Allow for configurable hosts, or truste client in some different ways
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://admin.socket.io",
+      "http://localhost:5000",
+      "null",
     ],
     credentials: true,
-  }
-})
-export default io
+  },
+});
+export default io;
 
 /**
  * Socket.io middleware to automatically retrieve device from namespaced endpoint in socket
- * @param {*} socket 
- * @param {*} next 
- * @returns 
+ * @param {*} socket
+ * @param {*} next
+ * @returns
  */
 export function useDeviceMiddleware(socket, next) {
-  let deviceId = getDeviceIdFromSocket(socket)
+  let deviceId = getDeviceIdFromSocket(socket);
   if (!deviceId)
-    return next(new Error('Missing device ID in socket namespace'))
-  let device = DeviceManager.shared.getDevice(deviceId)
+    return next(new Error("Missing device ID in socket namespace"));
+  let device = DeviceManager.shared.getDevice(deviceId);
   if (!device)
-    return next(new Error(`Socket unavailable. Device not found with id "${deviceId}"`))
-  
-  // Attach found device to socket
-  socket.device = device
+    return next(
+      new Error(`Socket unavailable. Device not found with id "${deviceId}"`)
+    );
 
-  return next()
+  // Attach found device to socket
+  socket.device = device;
+
+  return next();
 }
