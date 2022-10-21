7875d64e442c18f7ccecc10c491e25319a1ebd57
Fabio Borella <4450251+pencildrummer@users.noreply.github.com>
2022-10-21 16:55:41 +0200

    CSS fixes

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -1,14 +1,15 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-  mode: 'jit',
+  mode: "jit",
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    '../../packages/core-ui/lib/**/*.{js,ts,jsx,tsx}',
-    '../../packages/core-client/lib/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "../../packages/core-ui/lib/**/*.{js,ts,jsx,tsx}",
+    "../../packages/core-client/lib/**/*.{js,ts,jsx,tsx}",
     // These needs to be changed to allow styles included in plugins?
     // Or something more automatic instead adding every style for every plugin
-    //'../../packages/**/lib/**/*.{js,ts,jsx,tsx}',
+    "../../packages/**/client/**/*.{js,ts,jsx,tsx}",
+    "../../plugins/**/client/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -16,17 +17,15 @@ module.exports = {
         gray: colors.zinc,
       },
       fontSize: {
-        'xxs': '10px'
+        xxs: "10px",
       },
       fontFamily: {
-        'syncopate': ['"Syncopate"', 'sans-serif']
-      }
+        syncopate: ['"Syncopate"', "sans-serif"],
+      },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [
-    require("tailwindcss-radix")(),
-  ],
-}
+  plugins: [require("tailwindcss-radix")()],
+};
